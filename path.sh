065b2120ae5ef14a3d0f3a4fbc260f558b047d4c
Andre Kelpe <efeshundertelf@googlemail.com>
2010-04-17 12:49:53 +0200

    added apache ant

--- path.sh
+++ path.sh
@@ -1,5 +1,6 @@
 export M2_HOME=/home/fs111/tools/maven
-export PATH=$PATH:/$M2_HOME/bin:/home/fs111/tools/eclipse
+export ANT_HOME=/home/fs111/tools/ant
+export PATH=$PATH:/$M2_HOME/bin:/home/fs111/tools/eclipse:$ANT_HOME/bin
 
 export JVM_ROOT=/usr/lib/jvm
 export JAVA_HOME=$JVM_ROOT/java
