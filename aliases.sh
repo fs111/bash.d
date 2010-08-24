41d672f8912782352be61b68f12e99771db9f622
Andre Kelpe <efeshundertelf@googlemail.com>
2010-08-24 14:03:50 +0200

    added ll and play trick found via one thing well

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,6 @@
 alias ..="cd .."
 alias ltr="ls -ltr"
+alias ll="ls -l"
 alias g="gvim"
 alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
+alias play="mplayer -playlist <(find "$PWD" -type f | sort -n)"
