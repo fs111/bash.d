d16b9028c0c9ea576580821e59842bea1e5c12bb
Andre Kelpe <efeshundertelf@googlemail.com>
2010-08-24 14:39:30 +0200

    made the play alias smarter so that it only finds music files in the current working directory

--- aliases.sh
+++ aliases.sh
@@ -3,4 +3,4 @@ alias ltr="ls -ltr"
 alias ll="ls -l"
 alias g="gvim"
 alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
-alias play="mplayer -playlist <(find "$PWD" -type f | sort -n)"
+alias play='mplayer -playlist <(find "$PWD" -type f -iname "*.mp3" -o -iname "*.ogg" | sort -n)'
