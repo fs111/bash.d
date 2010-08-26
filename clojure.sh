ef2f65334ec7668420044709a0ce52343c2deaa0
Andre Kelpe <efeshundertelf@googlemail.com>
2010-08-27 00:38:10 +0200

    added clojure REPL setup with jline support

---
+++ clojure.sh
@@ -0,0 +1,4 @@
+CLOJURE_HOME=/home/fs111/tools/clojure
+JLINE_HOME=/home/fs111/tools/jline
+export CLASSPATH=$CLASSPATH:$CLOJURE_HOME/clojure.jar:$JLINE_HOME/jline.jar
+alias clj="java jline.ConsoleRunner clojure.main $*"
