7700525d80693e14c15e5bdd77322e9300e035ba
André Kelpe <andre.kelpe@tomtom.com>
2010-11-22 16:07:36 +0100

    fixed path for ubuntu

--- path.sh
+++ path.sh
@@ -2,7 +2,7 @@ export M2_HOME=$HOME/tools/maven
 export ANT_HOME=$HOME/tools/ant
 export PATH=$PATH:/$M2_HOME/bin:$HOME/tools/eclipse:$ANT_HOME/bin
 
-export JVM_ROOT=/usr/lib/jvm
-export JAVA_HOME=$JVM_ROOT/java
+export JVM_ROOT=/usr/
+export JAVA_HOME=$JVM_ROOT
 
 export PATH=$PATH:$JRUBY_HOME/bin:$HOME/.bash.d//utils
