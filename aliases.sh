25cab3e0bec2146aa2054dd941aaca1e8a9744f0
Andre Kelpe <efeshundertelf@googlemail.com>
2011-02-13 17:46:44 +0100

    added some distro trickery to get the aliases correct

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,13 @@ alias ll="ls -l"
 alias g="gvim"
 alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
 alias play='mplayer -playlist <(find "$PWD" -type f -iname "*.mp3" -o -iname "*.ogg" | sort -n)'
-alias ack=ack-grep
 alias jsonlint="python -m simplejson.tool"
-unalias alert
+
+
+DISTRIBUTION=$(lsb_release -i | cut -d\: -f 2 | tr -d "\t")
+
+if [ $DISTRIBUTION != "Fedora" ]
+    then
+    alias ack=ack-grep
+    unalias alert
+fi
