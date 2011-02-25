6e49ba7d55bde84003df2285255bd8dae96ebea0
André Kelpe <efeshundertelf@googlemail.com>
2011-02-25 10:59:51 +0100

    added new alias r for opening a file in a running gvim

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,7 @@ alias ..="cd .."
 alias ltr="ls -ltr"
 alias ll="ls -l"
 alias g="gvim"
+alias r="gvim --remote-silent"
 alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
 alias play='mplayer -playlist <(find "$PWD" -type f -iname "*.mp3" -o -iname "*.ogg" | sort -n)'
 alias jsonlint="python -m simplejson.tool"
