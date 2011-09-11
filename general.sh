04939e926d6d92dd3d915c718fdf9ecf79b63017
Andre Kelpe <efeshundertelf@googlemail.com>
2011-09-11 19:47:47 +0200

    tweaked the readline completion behavior following some good advice here: http://redd.it/giq9e

--- general.sh
+++ general.sh
@@ -1,2 +1,7 @@
 # general settings for bash etc. 
 export HISTSIZE=100000
+# these are readline modifications to tweak the tab completion
+bind 'set completion-ignore-case on'
+bind 'set completion-prefix-display-length 2'
+bind 'set completion-map-case on'
+
