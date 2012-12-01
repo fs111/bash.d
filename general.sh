71cd0d511bacd8da505ac1b93f3147b034f94764
Andre Kelpe <efeshundertelf@googlemail.com>
2012-12-01 10:46:11 +0100

    make sure all of the bash history is properly written to disk

--- general.sh
+++ general.sh
@@ -1,5 +1,6 @@
 # general settings for bash etc. 
 export HISTSIZE=100000
+export SAVEHIST0=100000
 # these are readline modifications to tweak the tab completion
 bind 'set completion-ignore-case on'
 bind 'set completion-prefix-display-length 2'
