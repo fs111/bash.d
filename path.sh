03c67e3c90bd3e6b339cbb88e01649668fc2ab02
Andre Kelpe <efeshundertelf@googlemail.com>
2013-08-16 14:13:29 +0200

    set HADOOP_HOME

--- path.sh
+++ path.sh
@@ -7,6 +7,8 @@ export PATH=$PATH:$JRUBY_HOME/bin:$HOME/.bash.d//utils
 export GO_HOME=$HOME/code/go
 export GOROOT=$GO_HOME
 export PATH=$GO_HOME/bin/:$PATH
+export GOPATH=$HOME/tools/go
+export PATH=$GOPATH/bin:$PATH
 
 export PATH=$PATH:$HOME/.rvm/bin
 
@@ -14,8 +16,12 @@ export GRADLE_HOME=$HOME/tools/gradle
 export PATH=$GRADLE_HOME/bin:$PATH
 
 export HADOOP_PREFIX=$HOME/tools/hadoop
+export HADOOP_HOME=$HOME/tools/hadoop
 export PATH=$PATH:$HADOOP_PREFIX/bin
 
 export IVY_HOME=$HOME/tools/ivy
 
-export JAVA_HOME=/usr/lib/jvm/java-openjdk
+export JAVA_HOME=/opt/jdk1.6.0_45
+
+export EMR_HOME=$HOME/tools/emr
+export PATH=$EMR_HOME:$PATH
