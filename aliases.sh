be62f81095e3e9c5e23a92185e1eb4a41ee4a595
Andre Kelpe <efeshundertelf@googlemail.com>
2015-01-22 17:38:33 -0800

    java setup on OS X

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
-alias ls="ls --color=auto"
+alias ls="ls -G"
 alias ..="cd .."
 alias ltr="ls -ltr"
 alias ll="ls -l"
@@ -7,14 +7,5 @@ alias r="gvim --remote-silent"
 alias eoip="ssh -2 -N -l fs111 -D7070 83.169.35.107 -p 443"
 alias play='mplayer -playlist <(find "$PWD" -type f -iname "*.mp3" -o -iname "*.ogg" -o -iname "*.flac" | sort -n)'
 alias jsonlint="python -m simplejson.tool"
-alias yup="yum -y"
-alias crs="kill -10 $(pidof resizR)"
 
-
-DISTRIBUTION=$(lsb_release -i | cut -d\: -f 2 | tr -d "\t")
-
-if [ $DISTRIBUTION != "Fedora" ]
-then
-    alias ack=ack-grep
-fi
-unset DISTRIBUTION
+#OSX specific
---
+++ osx.sh
@@ -0,0 +1,5 @@
+export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
+
+alias java6='export JAVA_HOME=$(/usr/libexec/java_home -v "1.6.0")'
+alias java7='export JAVA_HOME=$(/usr/libexec/java_home -v "1.7.0")'
+alias java8='export JAVA_HOME=$(/usr/libexec/java_home -v "1.8.0")'
