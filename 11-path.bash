ac0224f71eb8720b0e000dda916d83d11b4d8dbc
André Kelpe <akelpe@salesforce.com>
2022-10-28 15:44:54 +0200

    bye bye hadoop, it was a good time while it lasted

--- 11-path.bash
+++ 11-path.bash
@@ -4,9 +4,6 @@ export PATH=$M2_HOME/bin:$PATH
 export GRADLE_HOME=$HOME/tools/gradle
 export PATH=$GRADLE_HOME/bin:$PATH
 
-export HADOOP_PREFIX=$HOME/tools/hadoop
-export PATH=$PATH:$HADOOP_PREFIX/bin
-
 export PATH=$HOME/.local/bin:$PATH
 
 export GOPATH=~/code/thirdparty/go
