a1635f74354615dfcc4c06270bb48987ced37362
Andre Kelpe <git@kel.pe>
2023-02-15 13:15:12 +0100

    restore ls colors on linux

--- 20-aliases.bash
+++ 20-aliases.bash
@@ -1,7 +1,10 @@
-alias ls="ls -G"
+alias ls="ls -G --color=auto"
 alias ..="cd .."
 alias ltr="ls -ltr"
 alias ll="ls -l"
 alias jsonlint="python -m json.tool"
 alias t="tmux attach || tmux"
 alias e="fzf --preview 'bat --theme Dracula --color=always {}' | xargs -n1 -o $EDITOR"
+if [ -f "/etc/debian_version" ]; then
+    alias e="fzf --preview 'batcat --theme Dracula --color=always {}' | xargs -n1 -o $EDITOR"
+fi
