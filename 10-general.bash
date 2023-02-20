9b283dcf33f0698a84caeffa1b951c6ef25063d9
André Kelpe <akelpe@salesforce.com>
2023-02-20 10:54:13 +0100

    check for hstr only after the PATH is fully constructed

--- 10-general.bash
+++ 10-general.bash
@@ -1,19 +1,9 @@
-if command -v hstr > /dev/null
-then
-    # HSTR configuration - add this to ~/.bashrc
-    alias hh=hstr                    # hh to be alias for hstr
-    export HSTR_CONFIG=hicolor,regexp-match   # get more colors, regex matching
-    shopt -s histappend              # append new history items to .bash_history
-    export HISTCONTROL=ignorespace   # leading space hides commands from history
-    export HISTFILESIZE=10000000     # increase history file size (default is 500)
-    export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
-    # ensure synchronization between bash memory and history file
-    export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
-    # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
-    if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
-    # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
-    if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
-fi
+shopt -s histappend              # append new history items to .bash_history
+export HISTCONTROL=ignorespace   # leading space hides commands from history
+export HISTFILESIZE=10000000     # increase history file size (default is 500)
+export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
+# ensure synchronization between bash memory and history file
+export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
 
 # skip history for one and two letter commands
 HISTIGNORE='?:??'
--- 60-util.bash
+++ 60-util.bash
@@ -14,3 +14,13 @@ then
     eval "$(direnv hook bash)"
 fi
 
+if command -v hstr > /dev/null
+then
+    export HSTR_CONFIG=hicolor,regexp-match   # get more colors, regex matching
+    alias hh=hstr                    # hh to be alias for hstr
+    # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
+    if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
+    # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
+    if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
+fi
+
