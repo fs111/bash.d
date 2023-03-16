73326adb106f8acc19ee9acadfd57aa40fe037a0
André Kelpe <akelpe@salesforce.com>
2023-03-16 10:27:06 +0100

    e alias: expand $EDITOR at runtime not at definition time

--- 20-aliases.bash
+++ 20-aliases.bash
@@ -4,7 +4,7 @@ alias ltr="ls -ltr"
 alias ll="ls -l"
 alias jsonlint="python -m json.tool"
 alias t="tmux attach || tmux"
-alias e="fzf --preview 'bat --theme Dracula --color=always {}' | xargs -n1 -o $EDITOR"
+alias e='fzf --preview "bat --theme Dracula --color=always {}" | xargs -n1 -o $EDITOR'
 if [ -f "/etc/debian_version" ]; then
-    alias e="fzf --preview 'batcat --theme Dracula --color=always {}' | xargs -n1 -o $EDITOR"
+    alias e='fzf --preview "batcat --theme Dracula --color=always {}" | xargs -n1 -o $EDITOR'
 fi
