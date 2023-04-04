24b2887c6e8d2ca0f96cd0c0e6701bc8404eca84
André Kelpe <akelpe@salesforce.com>
2023-04-04 10:53:54 +0200

    improved e alias

--- 20-aliases.bash
+++ 20-aliases.bash
@@ -4,7 +4,7 @@ alias ltr="ls -ltr"
 alias ll="ls -l"
 alias jsonlint="python -m json.tool"
 alias t="tmux attach || tmux"
-alias e='fzf --preview "bat --theme Dracula --color=always {}" | xargs -n1 -o $EDITOR'
 if [ -f "/etc/debian_version" ]; then
-    alias e='fzf --preview "batcat --theme Dracula --color=always {}" | xargs -n1 -o $EDITOR'
+    alias bat='batcat'
 fi
+alias e='fzf --print0 --preview "bat --theme Dracula --color=always {}" | xargs -0 -o $EDITOR'
