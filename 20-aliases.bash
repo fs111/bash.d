c8350228d04d7d437516f67fd7780c2463267a72
André Kelpe <akelpe@salesforce.com>
2023-08-15 13:26:17 +0200

    enable multi-selection with key bindings for e alias

--- 20-aliases.bash
+++ 20-aliases.bash
@@ -7,4 +7,4 @@ alias t="tmux attach || tmux"
 if [ -f "/etc/debian_version" ]; then
     alias bat='batcat'
 fi
-alias e='fzf --print0 --preview "bat --theme Dracula --color=always {}" | xargs -0 -o $EDITOR'
+alias e='fzf -m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all --print0 --preview "bat --theme Dracula --color=always {}" | xargs -0 -o $EDITOR'
