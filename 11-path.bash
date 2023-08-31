e24cd3365456515ad29a9a7196cf92af362584cd
André Kelpe <akelpe@salesforce.com>
2023-08-31 10:56:58 +0200

    make sure $HOME/.local/bin is first in the path on macos

--- 11-path.bash
+++ 11-path.bash
@@ -1,3 +1,3 @@
+[ -d /opt/local/bin ] && export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export PATH=$HOME/.local/bin:$PATH
-
 export GOPATH=~/code/thirdparty/go
--- 15-osx.bash
+++ 15-osx.bash
@@ -1,7 +1,5 @@
 if [[ $(uname -s) == "Darwin" ]] 
 then
-  export JAVA_TOOL_OPTIONS='-Djava.awt.headless=true'
-
 # fix for mosh being confused by default locale
   export LANG=en_GB.UTF-8 LC_CTYPE=en_GB.UTF-8
 
@@ -12,8 +10,6 @@ then
   if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
       . /opt/local/etc/profile.d/bash_completion.sh
   fi
-
-  export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 
   alias ls="gls --color=auto"
 fi
