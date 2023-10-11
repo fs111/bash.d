dc65e96eb6e5f723ccd5476804317e1e4f8153a3
Andre Kelpe <git@kel.pe>
2023-10-11 12:10:59 +0200

    move l alias to 20-aliases.bash; disable zoxide

--- 20-aliases.bash
+++ 20-aliases.bash
@@ -3,7 +3,10 @@ alias ltr="ls -ltr"
 alias ll="ls -l"
 alias jsonlint="python -m json.tool"
 alias t="tmux attach || tmux"
+export BAT_THEME="Dracula"
 if [ -f "/etc/debian_version" ]; then
     alias bat='batcat'
+    alias eza='exa'
 fi
+alias l="eza -l --header --git --icons"
 alias e='fzf -m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all --print0 --preview "bat --theme Dracula --color=always {}" | xargs -0 -o $EDITOR'
--- 99-staging.bash
+++ 99-staging.bash
@@ -1,8 +1,1 @@
-if command -v zoxide &> /dev/null
-then
-    eval "$(zoxide init bash)"
-fi
 
-alias l="eza -l --header --git --icons"
-
-export BAT_THEME="Dracula"
