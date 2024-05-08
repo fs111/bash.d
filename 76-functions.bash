d1562b349821251443b88c60915130b3de5a87e2
André Kelpe <akelpe@salesforce.com>
2024-05-08 16:38:58 +0200

    simple current time in various time-zones I care about

--- 76-functions.bash
+++ 76-functions.bash
@@ -5,3 +5,12 @@ function crt {
 function sc {
     show-cert "$1" | jless
 }
+
+function tt {
+    TIME_ZONES="UTC Europe/Berlin Europe/Dublin India/Hyderabad America/Los_Angeles America/New_York"
+
+    for tz in ${TIME_ZONES}
+    do
+        printf "%s: %s\n" "${tz}" "$(TZ="${tz}" date +%H:%M)"
+    done | column -t
+}
