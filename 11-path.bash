7230c40ac8a7cc514e081fe78918a8b85095850d
André Kelpe <akelpe@salesforce.com>
2024-05-08 16:37:33 +0200

    put GOPATH/bin in front of PATH

--- 11-path.bash
+++ 11-path.bash
@@ -1,4 +1,4 @@
 [ -d /opt/local/bin ] && export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 export GOPATH=~/code/thirdparty/go
-[ -d "$GOPATH"/bin ] && export PATH=$PATH:$GOPATH/bin
+[ -d "$GOPATH"/bin ] && export PATH=$GOPATH/bin:$PATH
