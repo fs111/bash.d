51df7d2decc0a0a3476481883d7efd3de6e56aa1
Andre Kelpe <akelpe@salesforce.com>
2024-09-11 11:24:30 +0200

    clean up EDITOR setup on mac

--- 15-osx.bash
+++ 15-osx.bash
@@ -3,10 +3,6 @@ then
 # fix for mosh being confused by default locale
   export LANG=en_GB.UTF-8 LC_CTYPE=en_GB.UTF-8
 
-  alias mvim="/Applications/MacVim.app/Contents/MacOS/Vim -g"
-  alias vim="mvim -v"
-  export EDITOR="/Applications/MacVim.app/Contents/MacOS/Vim -v"
-
   if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
       . /opt/local/etc/profile.d/bash_completion.sh
   fi
